0d50184b826552d082fe31a96c317f345c6c6851
bernatpuig11 <61776677+bernatpuig11@users.noreply.github.com>
2021-05-19 17:53:39 +0200

    Update StartWorkout.swift

--- Views/StartWorkout.swift
+++ Views/StartWorkout.swift
@@ -9,82 +9,15 @@ import SwiftUI
 import FirebaseDatabase
 
 struct StartWorkout: View {
+    @State var level: String
+    @State var minutes: Int
     
-    @ObservedObject private var viewModel = ListExerciseViewModel()
+    @State var exercises: [String] = []
     
-    @State var minutes1: Int = 0
-    @State var seconds1: Int = 0
-    @State var minutes2: Int = 2
-    @State var seconds2: Int = 0
-    @State var timerIsPaused1: Bool = true
-    @State var timerIsPaused2: Bool = true
-    @State var text1 = ""
-    @State var timer1: Timer? = nil
-    @State var timer2: Timer? = nil
-    
-    var exercises: [String]
     
     var body: some View {
         VStack {
-            HStack {
-                Spacer().frame(width: 200, height: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                if minutes1 < 10 && seconds1 < 10{
-                    Text("0\(minutes1):0\(seconds1)")
-                        .font(.title)
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(.white)
-                        .frame(minWidth: 0, maxWidth: 120,minHeight: 50)
-                        .background(Color.blue)
-                        .cornerRadius(10)
-                        .font(.title)
-                        .padding(.all)
-                }
-                else if minutes1 < 10 {
-                    Text("0\(minutes1):\(seconds1)")
-                        .font(.title)
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(.white)
-                        .frame(minWidth: 0, maxWidth: 120,minHeight: 50)
-                        .background(Color.blue)
-                        .cornerRadius(10)
-                        .font(.title)
-                        .padding(.all)
-                }
-                else if seconds1 < 10 {
-                    Text("\(minutes1):0\(seconds1)")
-                        .font(.title)
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(.white)
-                        .frame(minWidth: 0, maxWidth: 120,minHeight: 50)
-                        .background(Color.blue)
-                        .cornerRadius(10)
-                        .font(.title)
-                        .padding(.all)
-                }
-                else  {
-                    Text("\(minutes1):\(seconds1)")
-                        .font(.title)
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(.white)
-                        .frame(minWidth: 0, maxWidth: 120,minHeight: 50)
-                        .background(Color.blue)
-                        .cornerRadius(10)
-                        .font(.title)
-                        .padding(.all)
-                }
-                
-            }.position(x: 200, y: 80)
-            
-            Button(action:{
-                if timerIsPaused1 {
-                    startTimer1()
-                }
-                if timerIsPaused2 {
-                    startTimer2()
-                }
-                print("NEXT")
-            }){
-                Text("START WORKOUT")
+            Text("START WORKOUT")
                     .font(.title)
                     .multilineTextAlignment(.center)
                     .foregroundColor(.white)
@@ -93,84 +26,14 @@ struct StartWorkout: View {
                     .cornerRadius(10)
                     .font(.title)
                     .padding(.all)
-            }
             
-            Spacer().frame(width: 200, height: 30, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            
-            if viewModel.exerc.count > 0 {
-                let exercise = viewModel.exerc[0]
-                Image(exercise.image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .cornerRadius(5)
-                    .padding(.all)
-            }
-            else {
-                Image("Lunges")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .cornerRadius(5)
-                    .padding(.all)
-            }
-            
-            if minutes2 < 10 && seconds2 < 10{
-                Text("0\(minutes2):0\(seconds2)")
-                    .font(.title)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.white)
-                    .frame(minWidth: 0, maxWidth: 120,minHeight: 50)
-                    .background(Color.black)
-                    .cornerRadius(10)
-                    .font(.title)
-                    .padding(.all)
-            }
-            else if minutes2 < 10 {
-                Text("0\(minutes2):\(seconds2)")
-                    .font(.title)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.white)
-                    .frame(minWidth: 0, maxWidth: 120,minHeight: 50)
-                    .background(Color.black)
-                    .cornerRadius(10)
-                    .font(.title)
-                    .padding(.all)
-            }
-            else if seconds2 < 10 {
-                Text("\(minutes2):0\(seconds2)")
-                    .font(.title)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.white)
-                    .frame(minWidth: 0, maxWidth: 120,minHeight: 50)
-                    .background(Color.black)
-                    .cornerRadius(10)
-                    .font(.title)
-                    .padding(.all)
-            }
-            else {
-                Text("\(minutes2):\(seconds2)")
-                    .font(.title)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.white)
-                    .frame(minWidth: 0, maxWidth: 120,minHeight: 50)
-                    .background(Color.black)
-                    .cornerRadius(10)
-                    .font(.title)
-                    .padding(.all)
-            }
             
             Spacer().frame(width: 200, height: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             
             HStack {
-                Spacer()
-                Button(action:{
-                    self.stopTimer2()
-                    self.resetTimer(x: 2)
-                    self.minutes2 = 2
-                    print("NEXT")
-                }){
-                    NavigationLink(destination: ExerciseWorkout(minutes1: minutes1, seconds1: seconds1, exercises: exercises)){
-                        
-                            Image(systemName: "arrow.right")
+                NavigationLink(destination: ExerciseWorkout(exercises: exercises)){
+                        let _ = print("Exercises \(exercises)")
+                        Image(systemName: "arrow.right")
                                 .resizable()
                                 .frame(width: 40, height: 30, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                                 .aspectRatio(contentMode: .fit)
@@ -178,76 +41,57 @@ struct StartWorkout: View {
                                 .background(Color.blue)
                                 .cornerRadius(10)
                                 .foregroundColor(Color.white)
-                    }
                 }
             }
         }.background(Image("image"))
         .edgesIgnoringSafeArea([.top])
-        .onAppear(){viewModel.fetchData()}
+        .onAppear(){fetch()}
     }
     
-    func startTimer1(){
-        timerIsPaused1 = false
-        timer1 = Timer.scheduledTimer(withTimeInterval: 1, repeats: true){ tempTimer in
-            if self.seconds1 == 59 {
-                self.seconds1 = 0
-                if self.minutes1 == 59 {
-                    self.minutes1 = 0
-                }
-                else {
-                    self.minutes1 = self.minutes1 + 1
-                }
-            }
-            else {
-                self.seconds1 = self.seconds1 + 1
-            }
+    private func fetch(){
+        let _ = print("Level \(level) and minutes \(minutes)")
+        if level == "Easy" && minutes == 20 {
+            self.exercises = ["Shake shoulders","Australian chin ups","Parcial Australian chin ups","Australian Pull ups","Burpees","Crunch lumbar","Running in site"]
         }
-    }
-    
-    func startTimer2(){
-        timerIsPaused2 = false
-        timer2 = Timer.scheduledTimer(withTimeInterval: 1, repeats: true){ tempTimer in
-            if self.seconds2 == 0 {
-                self.seconds2 = 59
-                if self.minutes2 == 0 {
-                    self.minutes2 = 59
-                }
-                else {
-                    self.minutes2 = self.minutes2 - 1
-                }
-            }
-            else {
-                self.seconds2 = self.seconds2 - 1
-            }
+        else if level == "Easy" && minutes == 30 {
+            self.exercises = ["Push ups","Squat Calf-Raise","Stiff-Leg Ankle Hop","Wall Sit","Sumo Squat","Reverse Lunge","Half burpees","Hollow body tucked crunches","Isometric knee raises","Isometic push ups"]
         }
-    }
-    
-    func stopTimer1(){
-        timerIsPaused1 = true
-        timer1?.invalidate()
-        timer1 = nil
-    }
-    
-    func stopTimer2(){
-        timerIsPaused2 = true
-        timer2?.invalidate()
-        timer2 = nil
-    }
-    
-    func resetTimer(x: Int){
-        if x == 1 {
-            self.minutes1 = 0
-            self.seconds1 = 0
+        else if level == "Easy" && minutes == 40 {
+            self.exercises = ["Shake shoulders","Australian chin ups","Parcial Australian chin ups","Push ups","Squat Calf-Raise","Stiff-Leg Ankle Hop","Wall Sit","Sumo Squat","Reverse Lunge","Half burpees","Hollow body tucked crunches","Isometric knee raises","Holding Knee raises"]
+        }
+        else if level == "Easy" && minutes == 50 {
+            self.exercises = ["Leg raises on the floor","Mountain climbers","Planche body","Shoulder taps","Tucked hollow body hold","Short dips","Bank dips","Negative dips","Femoral curl","Isometric squat","Explosive squats","Squat Calf-Raise","Stiff-Leg Ankle Hop","Wall Sit","Sumo Squat","Reverse Lunge","Isometic push ups"]
+        }
+        else if level == "Medium" && minutes == 20 {
+            self.exercises = ["Asymmetric pull ups","Negative pull ups","Prone chin-ups","Neutral pull-ups","Supine pull-ups","Leg raises","Dragon flag"]
+        }
+        else if level == "Medium" && minutes == 30 {
+            self.exercises = ["Hollow body hold","Knee raises with hold","Pull over","Hold tuck front lever","L sit raises","Explosive squats","Bupees","Decline push-ups","Bulgarian squat","Airborne squat",]
+        }
+        else if level == "Medium" && minutes == 40 {
+            
+        }
+        else if level == "Medium" && minutes == 50 {
+            
+        }
+        else if level == "Hard" && minutes == 20 {
+            self.exercises = ["Pseudo push-ups","Explosive bulgarian squat","Pistol squat","Hold advance planche","Muscle up","Press to hanstand","Tucked planche push-ups"]
+        }
+        else if level == "Hard" && minutes == 30 {
+            self.exercises = ["Frog to hanstand","Free hanstand push-ups","Hold advance planche","Muscle up","Press to hanstand","Dips","Korean dips","Headbangers","Leg raises","Skin the cat raises"]
+        }
+        else if level == "Hard" && minutes == 40 {
+            self.exercises = ["Diamond push-ups","Back lever pull-ups","Pseudo push-ups","Explosive bulgarian squat","Pistol squat","Front lever raises","Frog to hanstand","Free hanstand push-ups","Dips","Korean dips","Headbangers","Leg raises","Skin the cat raises"]
         }
         else {
-            self.minutes2 = 2
-            self.seconds2 = 0
+            self.exercises = ["Diamond push-ups","Back lever pull-ups","Pseudo push-ups","Explosive bulgarian squat","Pistol squat","Front lever raises","Frog to hanstand","Free hanstand push-ups","Hold advance planche","Muscle up","Press to hanstand","Tucked planche push-ups","Dips","Korean dips","Headbangers","Leg raises","Skin the cat raises"]
         }
     }
 }
 
 struct StartWorkout_Previews: PreviewProvider {
     static var previews: some View {
-        StartWorkout(exercises: [])
+        StartWorkout(level:"", minutes:0)
     }
 }
+
